8bd36a5b9cf0e31e0a41473667bdfcd36e2de781
hungtvph50704 <tranviethung315@gmail.com>
2025-06-12 17:07:47 +0700

    sua

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -31,7 +31,7 @@
         </div>
 
         <!-- Email Address -->
-        <div class="mt-4">f
+        <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
                 required autocomplete="username" />
