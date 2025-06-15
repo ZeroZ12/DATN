e774595e297fab5c2cbf3be77bb61dbc0c656cc3
Phonghehe1 <phongtvph52541@gmail.com>
2025-06-15 17:29:22 +0700

    huhu

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,12 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-<<<<<<< HEAD
-        // Tạo theo migration 0001_01_01_000000_create_users_table
-        User::factory(2)->create();
-        
-       
-=======
         $this->call([
             UserSeeder::class,
             ChipSeeder::class,
@@ -29,6 +23,5 @@ class DatabaseSeeder extends Seeder
             ThuongHieuSeeder::class,
             CategorySeeder::class,
         ]);
->>>>>>> origin
     }
 }
