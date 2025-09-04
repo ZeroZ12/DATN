00089f4222dbdb20caf64dbe1e6ac29350f46508
ZeroZ12 <longthph53584@gmail.com>
2025-09-04 21:13:04 +0700

    j

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -65,7 +65,7 @@ class HomeController extends Controller
             )
             ->orderByDesc('id')
             // ->get();
-            ->paginate(60)
+            ->paginate(100)
             ->withQueryString();
 
         $thuongHieus = ThuongHieu::all();
@@ -88,10 +88,10 @@ class HomeController extends Controller
         })
         ->paginate( 9);
 
-    $sanPhamBanChay = SanPham::orderByDesc('luot_mua')
-        ->limit(5)
-        ->pluck('id')
-        ->toArray();
+        $sanPhamBanChay = SanPham::orderByDesc('luot_mua')
+            ->limit(5)
+            ->pluck('id')
+            ->toArray();
 
         // dd($sanphams);
         return view('client.home', compact('activeSaleEvents','sanPhamBanChay','sanphams', 'thuongHieus', 'chips', 'gpus', 'rams', 'oCungs', 'danhMucs', 'banners','r_cates', 'b_cates'));
